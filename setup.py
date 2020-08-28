bcaea37fe351e5b11346797b647657d5662f116a
Maxime Faye (Serial SA) <Maxime.Faye1@ge.com>
2020-08-28 15:37:18 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="m2m-token",
-    version="0.0.1",
+    version="0.0.2",
     author="Maxime Faye",
     author_email="maxime.faye@gmail.com",
     description="Generates token for machine-to-machine authentication",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
